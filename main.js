cf1f3d6abae118921202190738ceaa2d2655743f
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-05-24 20:23:20 -0500

    Implement difference function to calculate absolute difference between two numbers

---
+++ main.js
@@ -0,0 +1,16 @@
+// Math.abs methed is used to get the absolute value of a number
+function difference(a, b) {
+  return Math.abs(a - b);// this will return the absolute value of the difference between a and b
+}
+
+console.log(difference(3, 5));
+// Expected output: 2
+
+console.log(difference(5, 3));
+// Expected output: 2
+
+console.log(difference(1.23456, 7.89012));
+// Expected output: 6.6555599999999995
+
+
+
